a8becc1a586c1bd5e4d60b3626acbc6a5459e83b
Clovin <fudeoagr12@gmail.com>
2026-03-14 23:03:34 -0300

    Implementando a classe ServiceDTO

--- manicure-api/src/main/java/br/com/davidds5/manicure_api/dto/ServiceDTO.java
+++ manicure-api/src/main/java/br/com/davidds5/manicure_api/dto/ServiceDTO.java
@@ -1,7 +1,17 @@
 package br.com.davidds5.manicure_api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ServiceDTO {
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer duration;
 }
