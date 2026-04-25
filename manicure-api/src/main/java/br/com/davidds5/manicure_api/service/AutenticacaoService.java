ecce4c93c526c78433d0cddc18d6af92b65c9812
Clovin <207490550+Davidds5@users.noreply.github.com>
2026-04-25 10:09:15 -0300

    recurso
    adicionar ProfessionalRepository e AutenticacaoService à arquitetura do projeto.

--- manicure-api/src/main/java/br/com/davidds5/manicure_api/service/AutenticacaoService.java
+++ manicure-api/src/main/java/br/com/davidds5/manicure_api/service/AutenticacaoService.java
@@ -1,11 +1,16 @@
 package br.com.davidds5.manicure_api.service;
 
+import br.com.davidds5.manicure_api.entity.ClientEntity;
+import br.com.davidds5.manicure_api.entity.ProfessionalEntity;
 import br.com.davidds5.manicure_api.repository.ClientRepository;
+import br.com.davidds5.manicure_api.repository.ProfessionalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AutenticacaoService implements UserDetailsService {
@@ -17,10 +22,19 @@ public class AutenticacaoService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username)throws UsernameNotFoundException{
-        return clientRepository.findByEmail(username)
-        .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
-    }
     
+        Optional<ClientEntity> client = clientRepository.findByEmail(username);
+        if(client.isPresent()){
+            return (UserDetails) client.get();
+        }
+
+        Optional<ProfessionalEntity> professional = professionalRepository.findByEmail(username);
+        if(professional.isPresent()){
+            return (UserDetails) professional.get();
+        }
+        throw new UsernameNotFoundException("Usuário não encontrado");
+    }
+
     
     
 
