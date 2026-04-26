18bd8cc278458f40e2a2c9f4f5dd9f9854ec6d6e
Clovin <207490550+Davidds5@users.noreply.github.com>
2026-04-26 10:18:00 -0300

    .\.git\92

---
+++ manicure-api/src/main/java/br/com/davidds5/manicure_api/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package br.com.davidds5.manicure_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    // Responsável por criptografar e descriptografar senhas
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+
+                .csrf(csrf -> csrf.disable())
+                // Desativa a proteção contra ataques que não fazem sentido para uma API
+            
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    req.anyRequest().authenticated();
+                })
+                .build();
+    }
+
+    @Bean
+    // Gerencia a autenticação, responsável por verificar as credenciais do usuário
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+}
