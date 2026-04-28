e7aea0a422f92144a62160d67411216050086cfe
Clovin <207490550+Davidds5@users.noreply.github.com>
2026-04-28 00:30:20 -0300

    feat:
    implementa validacao de tokens e estrutura inicial do filtro de seguranca

--- manicure-api/src/main/java/br/com/davidds5/manicure_api/config/SecurityConfig.java
+++ manicure-api/src/main/java/br/com/davidds5/manicure_api/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll();
                     req.anyRequest().authenticated();
                 })
                 .build();
